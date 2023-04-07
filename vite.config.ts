0bde04c43ce4986e783e16fdadea0c2083ae9c7d
realsarius <berkansozer@protonmail.com>
2023-04-07 10:52:11 +0300

    fixing deployment issues

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    extensions: ['.js', '.mjs'],
+  },
   plugins: [react()],
   base: '/odin-cv-application/',
 });
